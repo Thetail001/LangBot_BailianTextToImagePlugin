c85a15699211dd62c6be8c161aa24315d3fbe8e9
Thetail001 <56257172+Thetail001@users.noreply.github.com>
2025-02-18 08:06:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
-    model = "flux-dev"
-    DASHSCOPE_API_KEY = "YOUR_DASHSCOPE_API_KEY"
+    model = "flux-dev" #目前支持传入 "flux-schnell" 和 "flux-dev"。
+    size = "768*1024" #支持"512*1024, 768*512, 768*1024, 1024*576, 576*1024, 1024*1024"六种分辨率。
+    DASHSCOPE_API_KEY = "YOUR_DASHSCOPE_API_KEY" #阿里云百炼平台的API Key。
